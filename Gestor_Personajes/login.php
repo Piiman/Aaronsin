247dee17234b3216fa007bde3febed1faf4a5bd4
AureoGonz <aureogonz@hotmail.com>
2019-05-21 07:02:21 -0500

    modificación de login para que se pueda acceder y validar en la base de datos

--- Gestor_Personajes/login.php
+++ Gestor_Personajes/login.php
@@ -1,22 +1,56 @@
- <?php
-$servidor = "localhost";
-$user = "user";
-$pass = "1234";
-$basedatos = "dasebatos";
-
-$conn = new mysqli($servidor, $user, $pass, $basedatos);
-
-if ($conn->connect_error) {
-    die("Coneccion fallida " . $conn->connect_error);
+<?php session_start();
+if (isset($_SESSION['usuario'])) {
+	header('Location: formulario_personajes.php');
 }
-
-$sql = "SELECT $GET_["usuario"],$GET_["contrasena"] FROM usuarios";
-
-if ($conn->query($sql) === TRUE) {
-    echo "Inicio de sesion exitoso";
-} else {
-    echo "Usuario o contrase&ntildea incorrecta";
+$errores = '';
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	$usuario = filter_var(strtolower($_POST['usuario']), FILTER_SANITIZE_STRING);
+	$password = $_POST['password'];
+	$password = hash('sha512', $password);
+	try {
+		$conexion = new PDO('mysql:host=localhost;dbname=gsp', 'root', '');
+	} catch (PDOException $e) {
+		echo "Error:" . $e->getMessage();;
+	}
+	$statement = $conexion->prepare('
+		SELECT * FROM usuarios WHERE nombre = :usuario AND contrasena = :password'
+	);
+	$statement->execute(array(
+		':usuario' => $usuario, 
+		':password' => $password
+	));
+	$resultado = $statement->fetch();
+	if ($resultado !== false) {
+		$_SESSION['usuario'] = $usuario;
+		header('Location: formulario_personaje.php');
+	} else {
+		$errores .= '<li>Datos Incorrectos</li>';
+	}
 }
-
-$conn->close();
-?> 
+//require 'views/login.view.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<h1>Iniciar Sesión</h1>
+	<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST" class="formulario" name="login">
+		<input type="text" name="usuario" placeholder="Usuario"><br>
+		<input type="password" name="password" placeholder="Contrasena"><br>
+		<button type="button" onclick="login.submit()">Iniciar Sesion</button><br>
+		<?php if(!empty($errores)): ?>
+				<div>
+					<ul>
+						<?php echo $errores; ?>
+					</ul>
+				</div>
+		<?php endif; ?>
+	</form>
+	<p>
+		<a href="registro_usuario.php">Registrate</a><br>
+	</p>
+</body>
+</html>
