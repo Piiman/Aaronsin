cbab54b72cd8333cf266bf00fcc8d753f458fc3b
AureoGonz <aureogonz@hotmail.com>
2019-05-26 16:06:15 -0500

    Ya se pueden cargar personajes con imagen y todo

--- Gestor_Personajes/enviar_datos_personaje.php
+++ Gestor_Personajes/enviar_datos_personaje.php
@@ -14,28 +14,29 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 	try {
 	$conexion = new PDO('mysql:host=localhost;dbname=gsp', 'root', '');
-	if(isset($_FILES['img'])){
+	if(isset($_FILES['img'])&&!empty($_FILES['img']['name'])){
     	$nombre_archivo =$_FILES['img']['name'];
 	    $tipo_archivo = $_FILES['img']['type'];
     	$tamano_archivo = $_FILES['img']['size'];
     	if($tamano_archivo <=3000000 && $tamano_archivo>=1){
-    		if(tipo_archivo=="image/jpeg"||tipo_archivo=="image/jpg"||tipo_archivo=="image/png"||tipo_archivo=="image/gif"){
+    		if($tipo_archivo=="image/jpeg"||$tipo_archivo=="image/jpg"||$tipo_archivo=="image/png"||$tipo_archivo=="image/gif"){
 				$rutaDes = $_SERVER['DOCUMENT_ROOT']. "/img/" ;
 				move_uploaded_file($_FILES['img']['tmp_name'], $rutaDes . $nombre_archivo);	
 	    		$objetivo = fopen($rutaDes . $nombre_archivo, "r");
 	    		$contenido_foto = fread($objetivo, $tamano_archivo);
 				fclose($objetivo);
-    		//$statement = $conexion->prepare('SELECT * FROM personajes WHERE nombre = :nomper LIMIT 1');
-    		//$statement->execute(array(':usuario' => $nomper));
-			//$resultado = $statement->fetch();
-			//if ($resultado != false) {
-			//	$errores .= '<li>El nombre de usuario ya existe</li>';
-			//	header('Location: formulario_personaje_con_error.php');
-			//}else{
-			//	$statement = $conexion->prepare('INSERT INTO usuarios (id_per, autor, nombre,descripcion,foto,tfoto) VALUES (null, :idusu, :nombre, :descrip, :foto, :tfoto)');
-			//	$statement->execute(array(':idusu' => $idusuario, ':nombre' => $nomper, ':descrip'=>$desc, ':foto'=>$contenido_foto, ':tfoto'=>$tipo_archivo));
+    		$statement = $conexion->prepare('SELECT * FROM personajes WHERE nombre = :nomper LIMIT 1');
+    		$statement->execute(array(':usuario' => $nomper));
+			$resultado = $statement->fetch();
+			if ($resultado != false) {
+				$errores .= '<li>El personaje ya existe</li>';
+				header('Location: formulario_personaje_con_error.php');
+			}else{
+				$statement = $conexion->prepare('INSERT INTO personajes (id_per, autor, nombre,descripcion,foto,tfoto) VALUES (null, :idusu, :nombre, :descrip, :foto, :tfoto)');
+				$statement->execute(array(':idusu' => $idusuario, ':nombre' => $nomper, ':descrip'=>$desc, ':foto'=>$contenido_foto, ':tfoto'=>$tipo_archivo));
 
-			//header('Location: visualizacion.php');
+			header('Location: visualizacion.php');
+			}
 			}else{
 				$errores .= '<li>No es una imagen</li>';
 			//header('Location: formulario_personaje_con_error.php');
@@ -45,22 +46,22 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 			//header('Location: formulario_personaje_con_error.php');
 		}
 	}else{
-		//$statement = $conexion->prepare('SELECT * FROM personajes WHERE nombre = :nomper LIMIT 1');
-    	//$statement->execute(array(':usuario' => $nomper));
-		//$resultado = $statement->fetch();
-		//if ($resultado != false) {
-		//	$errores .= '<li>El nombre de usuario ya existe</li>';
-		//	header('Location: formulario_personaje_con_error.php');
-		//}else{
-		//	$statement = $conexion->prepare('INSERT INTO usuarios (id_per, autor, nombre,descripcion,foto,tfoto) VALUES (null, :idusu, :nombre, :descrip, null, null)');
-		//	$statement->execute(array(':idusu' => $idusuario, ':nombre' => $nomper, ':descrip'=>$desc));
-		//	}
-		//	header('Location: visualizacion.php');
+		$statement = $conexion->prepare('SELECT * FROM personajes WHERE nombre = :nomper LIMIT 1');
+    	$statement->execute(array(':usuario' => $nomper));
+		$resultado = $statement->fetch();
+		if ($resultado != false) {
+			$errores .= '<li>El personaje ya existe</li>';
+		}else{
+			$statement = $conexion->prepare('INSERT INTO personajes (id_per, autor, nombre,descripcion,foto,tfoto) VALUES (null, :idusu, :nombre, :descrip, null, null)');
+			$statement->execute(array(':idusu' => $idusuario, ':nombre' => $nomper, ':descrip'=>$desc));
+			}
+			header('Location: visualizacion.php');
 	}
 	} catch (PDOException $e) {
 		echo "Error:" . $e->getMessage();;
 	}}
 }
+unset($_FILES);
 
 	/*if(empty($nombre) && empty($desc)){
 		echo "Porfavor Llena todos los campos" ;
